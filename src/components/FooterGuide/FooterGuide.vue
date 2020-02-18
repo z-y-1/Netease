462d4f24a690ab02f28bcd12157157d395c9fed0
zy <987221532@qq.com>
2020-02-18 12:28:59 +0800

    'footGuide   construction'

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>FooterGuide</div>
+  <div id="footerContainer">
+      <div class="guideItem">
+          <i class="iconfont icon-shouye"></i>
+          <span>首页</span>
+      </div>
+      <div class="guideItem">
+          <i class="iconfont icon-fenlei"></i>
+          <span>分类</span>
+      </div>
+      <div class="guideItem">
+          <i class="iconfont icon-zhidemai"></i>
+          <span>值得买</span>
+      </div>
+      <div class="guideItem">
+          <i class="iconfont icon-qicheqianlian-"></i>
+          <span>购物车</span>
+      </div>
+      <div class="guideItem">
+          <i class="iconfont icon-profile"></i>
+          <span>我的</span>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +30,26 @@ export default {
 </script>
 
 <style lang="stylus">
- 
+@import '../../common/stylus/mixins.styl'
+    #footerContainer
+        display flex
+        justify-content space-around
+        width 100%
+        height 98px
+        position fixed
+        left 0
+        bottom 0
+        background #ffffff
+        // line-height 98px
+        .guideItem
+            margin-top 24px
+            line-height 35px
+            height 71px
+            text-align center
+            display flex
+            flex-direction column
+            
+            
+
+            
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const px2rem = require('postcss-px2rem');
+const px2rem = require('postcss-px2rem')
+// 配置postcs-px2rem
 const postcss = px2rem({
-    remUnit:37.5   //设计稿十等分之后的值
+  remUnit: 75   // 设计稿10等分之后的值
 })
 
 module.exports = {
